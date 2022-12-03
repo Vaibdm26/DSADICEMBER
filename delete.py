d1ecd22ce4447a645f8709dfbed506a81caa8e7a
VaibWeb <113412866+Vaibdm26@users.noreply.github.com>
2022-12-04 01:34:44 +0530

    day 3
    DSA Day3 Ques 4

---
+++ delete.py
@@ -0,0 +1,9 @@
+input_string = "tomato, Potato, teeth, total, Dentist, tattoo"
+result="" 
+for i in input_string: 
+   if i=='t': 
+       pass
+   else: 
+       result+=i 
+print("input string : ",input_string) 
+print("resultant string : ",result) 
